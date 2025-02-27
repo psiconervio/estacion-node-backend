99d7310ce8246e4c3b6b0657ca23bee91079c96f
psiconervio <psiconervio@gmail.com>
2025-02-27 20:44:55 -0300

    asa

--- front-jsx/vite.config.js
+++ front-jsx/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+  },
+  preview: {
+    port: 8080,
+    allowedHosts: ['estacion-node-backend.onrender.com'], // Mover aquí
   },
 });
